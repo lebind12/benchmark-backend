1193cb9c82687d45941ea29f548fc263c4145ac0
이우진 <52599002+lebind12@users.noreply.github.com>
2025-03-03 23:16:11 +0900

    [feat] 날짜별 전체리그 경기 반환 API 구현

--- domain/fixture/fixture_crud.py
+++ domain/fixture/fixture_crud.py
@@ -10,3 +10,9 @@ def get_fixtures_by_date_and_league(league_id: int, date: datetime, db:Session):
     return_data = db.query(Fixture).filter(Fixture.league_id == league_id, Fixture.date > date + timedelta(hours=6),\
         Fixture.date < date + timedelta(days=1, hours=6)).order_by(Fixture.date.asc()).all()
     return return_data
+
+def get_fixtures_by_date(date: datetime, db: Session):
+    start_date = date.replace(hour=6, minute=0, second=0, microsecond=0)
+    end_date = start_date + timedelta(days=1)
+    return_data = db.query(Fixture).filter(Fixture.date >= start_date, Fixture.date < end_date).order_by(Fixture.date.asc()).all()
+    return return_data
--- domain/fixture/fixture_router.py
+++ domain/fixture/fixture_router.py
@@ -16,5 +16,7 @@ def get_fixture_data(fixture_id: int = None, date:datetime = None, league_id:int
         return fixture_crud.get_fixture_data_by_id(fixture_id=fixture_id, db=db)
     elif date and league_id:
         return fixture_crud.get_fixtures_by_date_and_league(league_id=league_id, date=date, db=db)
+    elif date:
+        return fixture_crud.get_fixtures_by_date(date=date, db=db)
     else:
         raise HTTPException(status_code=400, detail="query not ok")
